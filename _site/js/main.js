a197e56e2fb888953040b8f67ecf621befb6a3a9
Kev Adsett <kev.adsett@gmail.com>
2013-11-14 14:18:27 +0000

    Removing bootstrap JS, adding Jquery, calculating age

---
+++ _site/js/main.js
@@ -0,0 +1,13 @@
+var millisecondsPerYear = 31449600000; // ignoring leap years
+
+$(document).on('ready', function() {
+    var age = getCurrentAge();
+    $('.what-i-do .age').html(age);
+});
+
+function getCurrentAge() {
+    var birthday = new Date("1986-10-26T22:28:00");
+    
+    var age = Math.floor((Date.now() - birthday) / millisecondsPerYear);
+    return age;
+}
